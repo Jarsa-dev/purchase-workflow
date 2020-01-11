129867ab71cbd9f8fc3a90beb6281c4d43e37c30
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2020-01-10 19:55:10 -0600

    [ADD] purchase_request_secondary_unit: new module to allow to use secondary units in purchase request

---
+++ purchase_request_secondary_unit/models/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2020 Jarsa
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+from . import purchase_request
---
+++ purchase_request_secondary_unit/wizard/__init__.py
@@ -0,0 +1,3 @@
+# Copyright 2020 Jarsa
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+from . import purchase_request_line_make_purchase_order
